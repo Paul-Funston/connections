4a03855f370a1c00d736413b5088c538efd224fd
Paul Funston <paulfunston9@gmail.com>
2022-12-16 10:43:36 -0600

    add feed and new posts

--- assets/script/profile.js
+++ assets/script/profile.js
@@ -57,3 +57,88 @@ function addUser(user) {
   document.querySelector('.contact-box').append(div);
 
 }
+
+
+// Post new content
+
+const newPostText = document.querySelector('textarea');
+const newPostImg = document.querySelector('.post-img');
+const newPostBtn = document.querySelector('.post-btn');
+const feed = document.querySelector('.feed-subgrid');
+
+
+newPostBtn.addEventListener('click', () => {
+  if (isFormValid()) {
+    createPost();
+    clearForm();
+  }
+});
+
+function isFormValid() {
+  return (newPostText.value.trim() !== '' || newPostImg.value !== '')
+};
+
+function clearForm() {
+  newPostText.value = '';
+  newPostImg.value = ''; 
+};
+
+function createPost() {
+  const newPost = newPostTemplate();
+  const postContent = createPostContent();
+  newPost.append(postContent);
+
+  feed.prepend(newPost);
+}
+
+
+  function newPostTemplate() {
+    const newPost = document.createElement('div'); 
+    const postHead = document.createElement('div');
+    const postHeadStart = document.createElement('div');
+    const posterAvatar = document.createElement('div');
+    const poster = document.createElement('p');
+    const postDate = document.createElement('p');
+    const currentDay = new Date();
+  
+    newPost.classList.add('post');
+    postHead.classList.add('post-head');
+    postHeadStart.classList.add('post-head-left');
+    posterAvatar.classList.add('avatar');
+    const avatarURL = './assets/media/avatar.png';
+
+    posterAvatar.style.backgroundImage = avatarURL;
+    postDate.innerText = currentDay.toDateString();
+    poster.innerText = 'Paul Funston';
+  
+    postHeadStart.append(posterAvatar, poster);
+    postHead.append(postHeadStart, postDate);
+    newPost.append(postHead);
+  
+    return newPost
+  }
+  
+  function createPostContent() {
+    const postContent = document.createElement('div');
+    const postText = document.createElement('p');
+    const postImg = document.createElement('figure');
+  
+    postContent.classList.add('post-content');
+    postText.innerText = newPostText.value.trim();
+    getImgData(postImg);
+    
+    postContent.append(postText, postImg);
+  
+    return postContent
+  }
+
+  function getImgData(postImg) {
+    const files = newPostImg.files[0];
+    if (files) {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(files);
+      fileReader.addEventListener("load", function () {
+        postImg.innerHTML = `<img src='${this.result}'>`;
+      });    
+    }
+  }
